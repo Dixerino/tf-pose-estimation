3656ade56634c20342aa7285d6394956509b3dfd
Jonathan Jiang <jonathanjiang99@gmail.com>
2019-07-18 15:08:52 +1000

    Jono's commit

--- src/assignment.py
+++ src/assignment.py
@@ -144,23 +144,25 @@ if __name__ == '__main__':
             # That is, an arm is above their head.
 <<<<<<< HEAD
             for k,v in human.body_parts.items():
-                (POSE_COCO_BODY_PARTS[k],v.y)
-                try:
+                (POSE_COCO_BODY_PARTS[k],v.y)       #repeatedly generates a list which correspsonds a body part in dictionary with its received y coordinate
+                try:                                #tests for specific k (body parts), code will only continue if these body parts are present
+                    k==5
+                    k==7                    
                     k==4
                     k==2
-                    if k==4:
-                        RWrist_y = v.y
-                    elif k==7:
-                        LWrist_y = v.y
-                    elif k==2:
-                        RShoulder_y = v.y
+                    if k==4:                        
+                        RWrist_y = v.y              #Y-coordinate of Right Wrist
+                    elif k==7:                      
+                        LWrist_y = v.y              #Y-coordinate of Left Wrist
+                    elif k==2:                          
+                        RShoulder_y = v.y           #Y-coordinate of Right Shoulder
                     elif k==5:
-                        LShoulder_y = v.y
-                    elif RWrist_y < RShoulder_y:
+                        LShoulder_y = v.y           #Y-coordinate of Left Shoulder
+                    elif RWrist_y < RShoulder_y:    #Tests if the right wrist is above the right shoulder
                         hail_taxi(image)
-                    elif LWrist_y < LShoulder_y:
+                    elif LWrist_y < LShoulder_y:    #Tests if the left wrist is above the left shoulder
                         hail_taxi(image)
-                except:
+                except:                             #If none of the body parts are present, don't run the if statement
                     print('No wrist or shoulder found')
 
 =======
@@ -181,4 +183,4 @@ if __name__ == '__main__':
             break
 
     cv2.destroyAllWindows()
- ASDF
+ 
